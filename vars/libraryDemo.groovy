795b3d28e743e45c331ea18274a85bedd3cc3fe0
Rick Ren <madongchn@163.com>
2020-11-22 20:16:46 +0800

    Update Jenkinsfile-k8sagent

--- vars/libraryDemo.groovy
+++ vars/libraryDemo.groovy
@@ -1,30 +1,41 @@
 #!groovy
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
-    pipeline{
-        agent {
-            kubernetes {
-            defaultContainer 'jnlp'
-            }
+def call(Map config) {
+    println(config)
+    pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
         }
-        stages {
-            stage('Example') {
-                //when {
-                //        buildingTag()
-                //    }
-                options {
-                    timeout(time: 1, unit: 'HOURS')
-                }
-                steps {
-                    echo 'Hello World ! I am in develop branch.'
-                    echo env.BRANCH_NAME
-                    sh 'printenv'
-                }
-            }
-        }
+      }
     }
+    // // evaluate the body block, and collect configuration into the object
+    // def pipelineParams= [:]
+    // body.resolveStrategy = Closure.DELEGATE_FIRST
+    // body.delegate = pipelineParams
+    // body()
+    // pipeline{
+    //     agent {
+    //         kubernetes {
+    //         defaultContainer 'jnlp'
+    //         }
+    //     }
+    //     stages {
+    //         stage('Example') {
+    //             //when {
+    //             //        buildingTag()
+    //             //    }
+    //             options {
+    //                 timeout(time: 1, unit: 'HOURS')
+    //             }
+    //             steps {
+    //                 echo 'Hello World ! I am in develop branch.'
+    //                 echo env.BRANCH_NAME
+    //                 sh 'printenv'
+    //             }
+    //         }
+    //     }
+    // }
  }
