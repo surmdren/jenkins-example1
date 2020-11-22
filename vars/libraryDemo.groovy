f7e99cc826be55be46e11fc6155bfab76079323f
Rick Ren <madongchn@163.com>
2020-11-22 20:52:58 +0800

    Update Jenkins-k8sagent

--- vars/libraryDemo.groovy
+++ vars/libraryDemo.groovy
@@ -1,31 +1,31 @@
 #!groovy
 def call(Map config) {
     println config
-    // // evaluate the body block, and collect configuration into the object
+    // evaluate the body block, and collect configuration into the object
     // def pipelineParams= [:]
     // body.resolveStrategy = Closure.DELEGATE_FIRST
     // body.delegate = pipelineParams
     // body()
-    // pipeline{
-    //     agent {
-    //         kubernetes {
-    //         defaultContainer 'jnlp'
-    //         }
-    //     }
-    //     stages {
-    //         stage('Example') {
-    //             //when {
-    //             //        buildingTag()
-    //             //    }
-    //             options {
-    //                 timeout(time: 1, unit: 'HOURS')
-    //             }
-    //             steps {
-    //                 echo 'Hello World ! I am in develop branch.'
-    //                 echo env.BRANCH_NAME
-    //                 sh 'printenv'
-    //             }
-    //         }
-    //     }
-    // }
+    pipeline{
+        agent {
+            kubernetes {
+            defaultContainer 'jnlp'
+            }
+        }
+        stages {
+            stage('Example') {
+                //when {
+                //        buildingTag()
+                //    }
+                options {
+                    timeout(time: 1, unit: 'HOURS')
+                }
+                steps {
+                    echo 'Hello World ! I am in develop branch.'
+                    echo env.BRANCH_NAME
+                    sh 'printenv'
+                }
+            }
+        }
+    }
  }
