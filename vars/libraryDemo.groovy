6136bc9f3d1489e4d8d3ff2961e87f423badbc7f
Rick Ren <madongchn@163.com>
2020-11-22 19:14:09 +0800

    Update Jenkinsfile-k8sagent

--- vars/libraryDemo.groovy
+++ vars/libraryDemo.groovy
@@ -20,7 +20,7 @@ def call(body) {
                     timeout(time: 1, unit: 'HOURS')
                 }
                 steps {
-                    echo 'Hello World ! I am in develop branch.'
+                    echo 'Hello World ! I am in develop branch, container kube'
                     echo env.BRANCH_NAME
                     sh 'printenv'
                 }
