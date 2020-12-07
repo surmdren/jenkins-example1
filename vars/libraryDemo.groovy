59ed1992794672f613e6d84cb9a89cec4cacf802
Rick Ren <madongchn@163.com>
2020-12-07 14:42:03 +0800

    Update with sleep 10

--- vars/libraryDemo.groovy
+++ vars/libraryDemo.groovy
@@ -21,7 +21,7 @@ def call(body) {
                     timeout(time: 1, unit: 'HOURS')
                 }
                 steps {
-                    sleep(10)
+                    sh 'sleep 10'
                     echo 'Hello World ! I am in develop branch.'
                     echo env.BRANCH_NAME
                     sh 'printenv'
