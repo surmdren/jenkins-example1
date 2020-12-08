b3a11a77fbb9d82982447f71ce6a49df51531c05
Rick Ren <madongchn@163.com>
2020-12-09 07:31:09 +0800

    Add projects Jenkinsfile-build

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,26 @@
-#!/usr/bin/groovy
+#!groovy
+def call(Map pipelineParams) {
+    println pipelineParams
+    pipeline{
+        agent {
+            kubernetes {
+            defaultContainer 'jnlp'
+            }
+        }
+        
+        triggers {       
+         pollSCM(env.BRANCH_NAME == 'master' ? '30 19 * * *' : '') // daily at 19:30 pm  --- QA  
+        }
 
-@Library('mymyUtilities') _
-
-build()
+        stages {
+            stage('Example') {
+                options {
+                    timeout(time: 1, unit: 'HOURS')
+                }
+                steps {
+                    echo 'Hello World ! I am in develop branch.'
+                    echo env.GIT_BRANCH
+                    sh 'printenv'
+                }
+            }
+        }
