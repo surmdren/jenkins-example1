c9423d8c710507a53ee91d8dc9cf4cd287a375c1
Rick Ren <madongchn@163.com>
2020-12-10 21:48:50 +0800

    Update buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -38,9 +38,7 @@ def call(Map pipelineParams) {
                     echo env.GIT_BRANCH
                     sh 'printenv'
                     sh 'sbt sbtVersion'
-                    sh "rm -rf web;git clone http://$gitlabaccount:$gitlabtoken@172.17.0.3/surmdren/web.git"
-                    sh 'cd web'
-                    sh 'docker build -t hkappdlv006.asia.pwcinternal.com:443/novus/novus-prod:env.BUILD_NUMBER web/'
+                    sh 'docker build -t hkappdlv006.asia.pwcinternal.com:443/novus/novus-prod:env.BUILD_NUMBER .'
                 }
             }
         }
